41635e85cae5b4b08042afa03e3188215d996111
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-09-30 02:44:02 +0000

    商品テーブルを作るSQLを追加しました

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -5,3 +5,8 @@ CREATE TABLE IF NOT EXISTS register_user(
     password_hash VARCHAR(255) NOT NULL,
     salt VARCHAR(128) NOT NULL
 );
+CREATE TABLE IF NOT EXISTS items(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    itemname VARCHAR(255) NOT NULL,
+    price INT NOT NULL
+);
