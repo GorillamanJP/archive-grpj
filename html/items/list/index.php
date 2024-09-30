25dd70283b28313c4329e8612c4b72fc44853ac0
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-09-30 05:10:13 +0000

    コードをフォーマットしてきれいにしました

--- html/items/list/index.php
+++ html/items/list/index.php
@@ -1,23 +1,24 @@
-
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/items/item.php";
 $item_obj = new Item();
-$items=$item_obj->get_all();
+$items = $item_obj->get_all();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>商品一覧</h1>
     <?php if (is_null($items)): ?>
         <p>商品はありません</p>
     <?php else: ?>
         <table>
-            <? foreach($items as $item): ?>
+            <? foreach ($items as $item): ?>
                 <tr>
                     <td><?= $item->get_itemname() ?></td>
                     <td><?= $item->get_price() ?></td>
@@ -26,4 +27,5 @@ $items=$item_obj->get_all();
         </table>
     <?php endif ?>
 </body>
+
 </html>
