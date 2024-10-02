50aff6bf6fbe3bc70c9b465baa84078fa7e13475
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-10-02 02:56:52 +0000

    誤字を見つけたので修正しました

--- html/items/update/index.php
+++ html/items/update/index.php
@@ -15,7 +15,7 @@ $item = $item->get_from_id($id);
 <body>
     <form action="update.php" method="post">
         <input type="hidden" name="id" value="<?= $item->get_id() ?>"></p>
-        <p><input type="text" name="item_name" id="itenmane" value="<?= $item->get_item_name() ?>">商品名</p>
+        <p><input type="text" name="item_name" id="item_name" value="<?= $item->get_item_name() ?>">商品名</p>
         <p><input type="number" name="price" id="price" value="<?= $item->get_price() ?>">価格</p>
         <input type="submit" value="更新">
     </form>
