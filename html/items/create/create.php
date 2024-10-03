3ff5ad75c2e71769984a8df5419870b3f71ca168
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-10-03 03:32:46 +0000

    内部的な処理のバグを修正しました

--- html/items/create/create.php
+++ html/items/create/create.php
@@ -2,20 +2,20 @@
 $item_name = htmlspecialchars($_POST["itenmane"]);
 $price = htmlspecialchars($_POST["price"]);
 $item_image = $_FILES["item_image"]["tmp_name"];
-$quantity = htmlspecialchars($_POST["quantity"]);
 
-# 商品
-require_once $_SERVER['DOCUMENT_ROOT'] . "/items/item.php";
-$item = new Item();
-$item = $item->create($item_name, $price, $item_image);
+if (create($item_name, $price, $item_image)) {
+}
 
-# 在庫
-require_once $_SERVER['DOCUMENT_ROOT'] . "/stocks/stock.php";
-$stock = new Stock();
-$stock = $stock->create($item->get_id(), $quantity);
+function create($item_name, $price, $item_image): bool
+{
+    # 商品
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/items/item.php";
+    $item = new Item();
+    $item = $item->create($item_name, $price, $item_image);
 
-if (!is_null($item) && !is_null($stock)) {
-    header("Location: ./success.html");
-} else {
-    header("Location: ./fail.html");
+    if (!is_null($item)) {
+        return true;
+    } else {
+        return false;
+    }
 }
