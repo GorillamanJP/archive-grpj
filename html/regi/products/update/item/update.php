1eabeb92fbf2a7ea6a1dcbd87b19e9e700ad17d1
T-Gorilla(Laptop) <js22007@anabuki-net.ne.jp>
2024-10-10 05:04:34 +0000

    入力保護をしました

--- html/regi/products/update/item/update.php
+++ html/regi/products/update/item/update.php
@@ -1,24 +1,28 @@
 <?php
 session_start();
+if (isset($_POST["id"], $_POST["item_name"], $_POST["price"], $_FILES["new_item_image"]["tmp_name"])) {
+    $id = htmlspecialchars($_POST["id"]);
+    $item_name = htmlspecialchars($_POST["item_name"]);
+    $price = htmlspecialchars($_POST["price"]);
+    $item_image = $_FILES["new_item_image"]["tmp_name"];
 
-$id = htmlspecialchars($_POST["id"]);
-$item_name = htmlspecialchars($_POST["item_name"]);
-$price = htmlspecialchars($_POST["price"]);
-$item_image = $_FILES["new_item_image"]["tmp_name"];
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/regi/items/item.php";
 
-require_once $_SERVER['DOCUMENT_ROOT'] . "/regi/items/item.php";
-
-try {
-    $item = new Item();
-    $item = $item->get_from_id($id);
-    $item = $item->update($item_name, $price, $item_image);
-    // 成功時のメッセージをセッションに保存
-    $_SESSION['message'] = "商品情報が正常に更新されました。";
-    $_SESSION['message_type'] = "success";  // 成功メッセージ用
-} catch (Exception $e) {
-    // 失敗時のメッセージをセッションに保存
-    $_SESSION['message'] = "エラーが発生しました: " . $e->getMessage();
-    $_SESSION['message_type'] = "error";  // エラーメッセージ用
+    try {
+        $item = new Item();
+        $item = $item->get_from_id($id);
+        $item = $item->update($item_name, $price, $item_image);
+        // 成功時のメッセージをセッションに保存
+        $_SESSION['message'] = "商品情報が正常に更新されました。";
+        $_SESSION['message_type'] = "success";  // 成功メッセージ用
+    } catch (Exception $e) {
+        // 失敗時のメッセージをセッションに保存
+        $_SESSION['message'] = "エラーが発生しました: " . $e->getMessage();
+        $_SESSION['message_type'] = "error";  // エラーメッセージ用
+    }
+} else {
+    $_SESSION["message"] = "入力情報が不足しています。";
+    $_SESSION["message_type"] = "error";
 }
-header("Location: /regi/products/list/index.php");
+header("Location: /regi/products/list/");
 exit();
