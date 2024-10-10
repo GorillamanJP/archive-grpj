8b031ec269b63f50beda4080fd950306733d111d
T-Gorilla(Laptop) <js22007@anabuki-net.ne.jp>
2024-10-10 04:29:08 +0000

    ログインチェック機能を実装しました

---
+++ html/regi/users/login_check.php
@@ -0,0 +1,23 @@
+<?php
+function login_check()
+{
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/regi/users/user.php";
+    session_start();
+    if (isset($_SESSION["login"]["user_id"])) {
+        try {
+            $user = new User();
+            $user = $user->get_from_id($_SESSION["login"]["user_id"]);
+        } catch (Exception $e) {
+            $_SESSION["error"]["message"] = "セッションエラー: ログインしなおしてください。";
+            $_SESSION["login"]["after"] = $_SERVER['REQUEST_URI'];
+            header("Location: /users/login/");
+            exit();
+        }
+    } else {
+        $_SESSION["error"]["message"] = "このページを表示するには、ログインが必要です。";
+        $_SESSION["login"]["after"] = $_SERVER['REQUEST_URI'];
+        header("Location: /users/login/");
+        exit();
+    }
+}
+login_check();
