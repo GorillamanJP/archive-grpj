55232da89337d38d022d1dc095d1a9b1240c32ea
takuya420 <js22034@anabuki-net.ne.jp>
2024-10-10 02:17:07 +0000

    入荷処理のパスを修正しました。

--- html/regi/products/update/stock/update.php
+++ html/regi/products/update/stock/update.php
@@ -15,7 +15,7 @@ try {
     $stock->commit();
     $_SESSION["message"] = "在庫が追加されました。";
     $_SESSION["message_type"] = "success";
-    header("Location: /products/list/index.php");
+    header("Location: /regi/products/list/index.php");
 } catch (Exception $e) {
     $_SESSION["message"] = "在庫の追加に失敗しました。";
     $_SESSION["message_type"] = "error";
