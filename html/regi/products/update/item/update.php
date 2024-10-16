57b4f123474ffd0cdc6677aacfd8e3918e9eac22
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-10-16 00:53:43 +0000

    商品を更新した際にエラーがあるときはリダイレクトを入力画面に行くようにしました

--- html/regi/products/update/item/update.php
+++ html/regi/products/update/item/update.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
-if (isset($_POST["id"], $_POST["item_name"], $_POST["price"], $_FILES["new_item_image"]["tmp_name"])) {
+if (!isset($_POST["id"])) {
+    $_SESSION["message"] = "商品のIDが指定されていません。\nこのメッセージが出る場合、内部のバグの可能性がありますので、「何を」「どのように」したらエラーが出たのかを開発者までお伝えください。\nご不便をおかけして申し訳ありませんが、ご協力をお願いします。";
+    $_SESSION["message_type"] = "danger";
+    header("Location ../../");
+    exit();
+}
+if (isset($_POST["item_name"], $_POST["price"], $_FILES["new_item_image"]["tmp_name"])) {
     $id = htmlspecialchars($_POST["id"]);
     $item_name = htmlspecialchars($_POST["item_name"]);
     $price = htmlspecialchars($_POST["price"]);
@@ -15,14 +21,19 @@ if (isset($_POST["id"], $_POST["item_name"], $_POST["price"], $_FILES["new_item_
         // 成功時のメッセージをセッションに保存
         $_SESSION['message'] = "商品情報が正常に更新されました。";
         $_SESSION['message_type'] = "success";  // 成功メッセージ用
+        header("Location: ../../list/");
+        exit();
     } catch (\Throwable $e) {
         // 失敗時のメッセージをセッションに保存
         $_SESSION['message'] = "エラーが発生しました: " . $e->getMessage();
-        $_SESSION['message_type'] = "error";  // エラーメッセージ用
+        $_SESSION['message_type'] = "danger";  // エラーメッセージ用
     }
 } else {
     $_SESSION["message"] = "入力情報が不足しています。";
-    $_SESSION["message_type"] = "error";
+    $_SESSION["message_type"] = "danger";
 }
-header("Location: /regi/products/list/");
-exit();
+?>
+<form action="./" method="post" id="post_form">
+    <input type="hidden" name="id" id="id" value="<?= $id ?>">
+</form>
+<script>document.getElementById("post_form").submit();</script>
