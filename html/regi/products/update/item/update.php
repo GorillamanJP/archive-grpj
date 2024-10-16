b0c30ca48bf241df02958ef07b71444cc541d795
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-10-16 01:06:07 +0000

    入荷処理も商品更新と同様にエラーがあるとメッセージがリダイレクトせず出るようになりました

--- html/regi/products/update/item/update.php
+++ html/regi/products/update/item/update.php
@@ -6,8 +6,10 @@ if (!isset($_POST["id"])) {
     header("Location ../../");
     exit();
 }
+
+$id = htmlspecialchars($_POST["id"]);
+
 if (isset($_POST["item_name"], $_POST["price"], $_FILES["new_item_image"]["tmp_name"])) {
-    $id = htmlspecialchars($_POST["id"]);
     $item_name = htmlspecialchars($_POST["item_name"]);
     $price = htmlspecialchars($_POST["price"]);
     $item_image = $_FILES["new_item_image"]["tmp_name"];
--- html/regi/products/update/stock/update.php
+++ html/regi/products/update/stock/update.php
@@ -1,29 +1,42 @@
 <?php
-$id = htmlspecialchars($_POST["id"]);
-$add_quantity = htmlspecialchars($_POST["add_quantity"]);
-
-require_once $_SERVER['DOCUMENT_ROOT'] . "/regi/stocks/stock.php";
-
 session_start();
-
-try {
-    $stock = new Stock();
-    $stock->start_transaction();
-    $stock = $stock->get_from_id($id);
-    $now_quantity = $stock->get_quantity();
-    // 在庫が0未満にならないようにチェック
-    $new_quantity = $now_quantity + $add_quantity;
-    if ($new_quantity < 0) {
-        throw new Exception("在庫が0未満になるため、更新できません。");
-    }
-
-    $stock = $stock->update($new_quantity);
-    $stock->commit();
-    $_SESSION["message"] = "在庫が追加されました。";
-    $_SESSION["message_type"] = "success";
-    header("Location: /regi/products/list/index.php");
-} catch (\Throwable $e) {
-    $_SESSION["message"] = $e->getMessage();
+if (!isset($_POST["id"])) {
+    $_SESSION["message"] = "商品のIDが指定されていません。\nこのメッセージが出る場合、内部のバグの可能性がありますので、「何を」「どのように」したらエラーが出たのかを開発者までお伝えください。\nご不便をおかけして申し訳ありませんが、ご協力をお願いします。";
     $_SESSION["message_type"] = "danger";
-    header("Location: /regi/products/list/index.php");
+    header("Location ../../");
+    exit();
 }
+
+$id = htmlspecialchars($_POST["id"]);
+
+if (isset($_POST["add_quantity"])) {
+    $add_quantity = htmlspecialchars($_POST["add_quantity"]);
+
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/regi/stocks/stock.php";
+
+    try {
+        $stock = new Stock();
+        $stock->start_transaction();
+        $stock = $stock->get_from_id($id);
+        $now_quantity = $stock->get_quantity();
+        // 在庫が0未満にならないようにチェック
+        $new_quantity = $now_quantity + $add_quantity;
+        if ($new_quantity < 0) {
+            throw new Exception("在庫が0未満になるため、更新できません。");
+        }
+
+        $stock = $stock->update($new_quantity);
+        $stock->commit();
+        $_SESSION["message"] = "在庫が追加されました。";
+        $_SESSION["message_type"] = "success";
+        header("Location: ../../list/");
+    } catch (\Throwable $e) {
+        $_SESSION["message"] = "エラーが発生しました: " . $e->getMessage();
+        $_SESSION["message_type"] = "danger";
+    }
+}
+?>
+<form action="./" method="post" id="post_form">
+    <input type="hidden" name="id" id="id" value="<?= $id ?>">
+</form>
+<script>document.getElementById("post_form").submit();</script>
