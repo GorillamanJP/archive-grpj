1eb7e19ce181e95328863ac3a51552417acb0290
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-10-16 03:29:38 +0000

    入力チェックとエラーメッセージの分岐を細かくしました

--- html/regi/products/update/item/update.php
+++ html/regi/products/update/item/update.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if ($_POST["id"] === "") {
+if (!isset($_POST["id"]) || $_POST["id"] === "") {
     $_SESSION["message"] = "商品のIDが指定されていません。\nこのメッセージが出る場合、内部のバグの可能性がありますので、「何を」「どのように」したらエラーが出たのかを開発者までお伝えください。\nご不便をおかけして申し訳ありませんが、ご協力をお願いします。";
     $_SESSION["message_type"] = "danger";
     header("Location ../../");
@@ -9,7 +9,20 @@ if ($_POST["id"] === "") {
 
 $id = htmlspecialchars($_POST["id"]);
 
-if (!($_POST["item_name"] === "" || $_POST["price"] === "" || $_FILES["new_item_image"]["tmp_name"] === "")) {
+if(!isset($_POST["item_name"]) || $_POST["item_name"] === ""){
+    # 商品名
+    $_SESSION["message"] = "商品名が空になっています。";
+    $_SESSION["message_type"] = "danger";
+} else if (!isset($_POST["price"]) || $_POST["price"] === ""){
+    # 価格
+    $_SESSION["message"] = "価格が空になっています。";
+    $_SESSION["message_type"] = "danger";
+} else if (!isset($_FILES["new_item_image"]["tmp_name"]) || $_FILES["new_item_image"]["tmp_name"] === ""){
+    # 画像
+    $_SESSION["message"] = "商品画像が空になっています。";
+    $_SESSION["message_type"] = "danger";
+} else {
+    # 全部入ってる
     $item_name = htmlspecialchars($_POST["item_name"]);
     $price = htmlspecialchars($_POST["price"]);
     $item_image = $_FILES["new_item_image"]["tmp_name"];
@@ -27,12 +40,10 @@ if (!($_POST["item_name"] === "" || $_POST["price"] === "" || $_FILES["new_item_
         exit();
     } catch (\Throwable $e) {
         // 失敗時のメッセージをセッションに保存
-        $_SESSION['message'] = "エラーが発生しました: " . $e->getMessage();
+        $_SESSION['message'] = "エラーが発生しました。";
+        $_SESSION["message_details"] = $e->getMessage();
         $_SESSION['message_type'] = "danger";  // エラーメッセージ用
     }
-} else {
-    $_SESSION["message"] = "入力情報が不足しています。";
-    $_SESSION["message_type"] = "danger";
 }
 ?>
 <form action="./" method="post" id="post_form">
