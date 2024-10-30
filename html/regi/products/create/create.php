a2f31658f51cf4bc8e2a82a8a61039189f89647b
kobayashi10101 <js22030@anabuki-net.ne.jp>
2024-10-30 06:14:22 +0000

    商品登録の際に在庫数を指定できるように

--- html/regi/products/create/create.php
+++ html/regi/products/create/create.php
@@ -12,6 +12,10 @@ if(!isset($_POST["price"]) || $_POST["price"] === ""){
     $_SESSION["message"] .= "「価格」";
     $ok = false;
 }
+if(!isset($_POST["add_quantity"]) || $_POST["add_quantity"] === ""){
+    $_SESSION["message"] .= "「在庫」";
+    $ok = false;
+}
 if(!isset($_FILES["item_image"]["tmp_name"]) || $_FILES["item_image"]["tmp_name"] === ""){
     $_SESSION["message"] .= "「商品画像」";
     $ok = false;
@@ -20,12 +24,13 @@ if(!isset($_FILES["item_image"]["tmp_name"]) || $_FILES["item_image"]["tmp_name"
 if ($ok) {
     $item_name = htmlspecialchars($_POST["item_name"]);
     $price = htmlspecialchars($_POST["price"]);
+    $quantity = htmlspecialchars($_POST["add_quantity"]);
     $item_image = $_FILES["item_image"]["tmp_name"];
 
     require_once $_SERVER['DOCUMENT_ROOT'] . "/regi/products/product.php";
     try {
         $product = new Product();
-        $product->create($item_name, $price, $item_image, 0);
+        $product->create($item_name, $price, $item_image, $quantity);
 
         // 成功メッセージをセッションに保存
         $_SESSION['message'] = '商品が正常に登録されました。';
