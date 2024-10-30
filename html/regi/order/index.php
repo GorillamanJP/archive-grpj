67be9848f444bd46dd4aac5d37f18ba245c5243a
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-10-30 00:39:50 +0000

    ユーザーを生成することをやめました

--- html/regi/order/index.php
+++ html/regi/order/index.php
@@ -14,8 +14,9 @@ session_start();
 <body>
     <h1>モバイルオーダーのレジ側トップ画面</h1>
     <h2>リンク生成ツール</h2>
-    <p>モバイルオーダーのリンク:<pre><span id="url_domain"></span>/order/?magic_char=<?= hash("SHA3-512", getenv("PASS_PHRASE")); ?></pre></p>
-    <p>このリンクを共有することでモバイルオーダーにアクセスできるようになります。</p>
+    <p>モバイルオーダーのリンク↓</p>
+    <p><pre><span id="url_domain"></span>/order/?magic_char=<?= hash("SHA3-512", getenv("PASS_PHRASE")); ?></pre></p>
+    <p>↑このリンクを共有することでモバイルオーダーにアクセスできるようになります。</p>
     <script>
         document.getElementById("url_domain").innerText = window.location.hostname;
     </script>
