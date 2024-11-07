4a1e873be9839eae56519466d244222864b6225f
takuya420 <js22034@anabuki-net.ne.jp>
2024-11-07 02:34:35 +0000

    モバイルオーダー会計一覧（作成中）

--- html/regi/order/list/index.php
+++ html/regi/order/list/index.php
@@ -20,54 +20,65 @@ $orders = $order_obj->get_all();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
         crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/common/list.css">
 </head>
 
 <body>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/common/alert.php"; ?>
-    <h1>注文一覧</h1>
-    <table>
-        <tr>
-            <th>注文番号</th>
-            <th>注文日時</th>
-            <th>詳細</th>
-            <th>受け取り</th>
-        </tr>
-        <?php if (is_null($orders)): ?>
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/common/navbar.php"; ?>
+    <div class="container mt-4">
+        <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/common/alert.php"; ?>
+        <h1 class="text-center mb-4">注文一覧</h1>
+        <p class="text-center my-3" style="font-size: 1.2em;">最終更新時刻:<span id="last-update">0000/0/0 00:00:00</span></p>
+        <div class="text-center mb-3">
+            <a href="../../" class="btn btn-outline-success btn-lg-custom p-2 mx-1">レジ画面へ</a>
+            <div class="alert alert-info mt-2">
+                詳細を見るには、項目を押してください。
+            </div>
+        </div>
+        <table>
             <tr>
-                <td colspan="4">
-                    <h2>受け取り待ちの注文はありません。</h2>
-                </td>
+                <th>注文番号</th>
+                <th>注文日時</th>
+                <th>詳細</th>
+                <th>受け取り</th>
             </tr>
-        <?php else: ?>
-            <?php foreach ($orders as $order): ?>
+            <?php if (is_null($orders)): ?>
                 <tr>
-                    <td><?= $order->get_order_order()->get_id() ?></td>
-                    <td><?= $order->get_order_order()->get_date() ?></td>
-                    <td>
-                        <table>
-                            <tr>
-                                <th>品名</th>
-                                <th>数量</th>
-                            </tr>
-                            <?php foreach ($order->get_order_details() as $detail): ?>
-                                <tr>
-                                    <td><?= $detail->get_item_name() ?></td>
-                                    <td><?= $detail->get_quantity() ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </table>
-                    </td>
-                    <td>
-                        <form action="../receive/" method="post">
-                            <input type="hidden" name="order_id" id="order_id"
-                                value="<?= $order->get_order_order()->get_id() ?>">
-                            <button type="submit">受け取り</button>
-                        </form>
+                    <td colspan="4">
+                        <h2>受け取り待ちの注文はありません。</h2>
                     </td>
                 </tr>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </table>
+            <?php else: ?>
+                <?php foreach ($orders as $order): ?>
+                    <tr>
+                        <td><?= $order->get_order_order()->get_id() ?></td>
+                        <td><?= $order->get_order_order()->get_date() ?></td>
+                        <td>
+                            <table>
+                                <tr>
+                                    <th>品名</th>
+                                    <th>数量</th>
+                                </tr>
+                                <?php foreach ($order->get_order_details() as $detail): ?>
+                                    <tr>
+                                        <td><?= $detail->get_item_name() ?></td>
+                                        <td><?= $detail->get_quantity() ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </table>
+                        </td>
+                        <td>
+                            <form action="../receive/" method="post">
+                                <input type="hidden" name="order_id" id="order_id"
+                                    value="<?= $order->get_order_order()->get_id() ?>">
+                                <button type="submit">受け取り</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </table>
+    </div>
 </body>
 
 </html>
