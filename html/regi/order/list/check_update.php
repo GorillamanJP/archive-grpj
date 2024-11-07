64fe0d1e968a44f8351595d758139fb75c538971
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-11-07 04:26:06 +0000

    セッションがないときに最終更新時刻がないとバグるのを修正しました

--- html/regi/order/list/check_update.php
+++ html/regi/order/list/check_update.php
@@ -7,6 +7,10 @@ if (!login_check()) {
 
 session_start();
 
+if (!isset($_SESSION["regi"]["order"]["list"]["last_update"]) || $_SESSION["regi"]["order"]["list"]["last_update"] === "") {
+    $_SESSION["regi"]["order"]["list"]["last_update"] = date("Y/m/d H:i:s");
+}
+
 $last_update = $_SESSION["regi"]["order"]["list"]["last_update"];
 
 try {
--- html/regi/order/list/updated_data.php
+++ html/regi/order/list/updated_data.php
@@ -7,10 +7,6 @@ if (!login_check()) {
 
 session_start();
 
-if (!isset($_SESSION["regi"]["order"]["list"]["last_update"]) || $_SESSION["regi"]["order"]["list"]["last_update"] === "") {
-    $_SESSION["regi"]["order"]["list"]["last_update"] = date("Y/m/d H:i:s");
-}
-
 $last_update = date("Y/m/d H:i:s");
 $_SESSION["regi"]["order"]["list"]["last_update"] = $last_update;
 
