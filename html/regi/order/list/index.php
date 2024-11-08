0c414da24b15246a1adbdd3a6ed99f70df08173e
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-11-08 04:44:08 +0000

    モバイルオーダーの自動更新を正しく適用しました

--- html/regi/order/list/index.php
+++ html/regi/order/list/index.php
@@ -47,51 +47,20 @@ $orders = $order_obj->get_all();
                     <th>受け取り</th>
                 </tr>
             </thead>
-            <tbody id="refresh">
+            <tbody id="table">
                 <tr>
                     <td colspan="4">
                         <h3>読み込み中…</h3>
                     </td>
                 </tr>
-            <?php else: ?>
-                <?php foreach ($orders as $order): ?>
-                    <tr>
-                        <td><?= $order->get_order_order()->get_id() ?></td>
-                        <td><?= $order->get_order_order()->get_date() ?></td>
-                        <td>
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>品名</th>
-                                        <th>数量</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php foreach ($order->get_order_details() as $detail): ?>
-                                        <tr>
-                                            <td><?= $detail->get_item_name() ?></td>
-                                            <td><?= $detail->get_quantity() ?></td>
-                                        </tr>
-                                    <?php endforeach; ?>
-                                <tbody>
-                            </table>
-                        </td>
-                        <td>
-                            <form action="../receive/" method="post">
-                                <input type="hidden" name="order_id" id="order_id"
-                                    value="<?= $order->get_order_order()->get_id() ?>">
-                                <input type='submit' value='受け取り' btn class='btn btn-outline-success round-button'>
-                            </form>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php endif; ?>
+            </tbody>
         </table>
     </div>
     <!-- 通知領域 -->
     <div id="notifications" class="toast-container position-fixed bottom-0 end-0 p-3"></div>
-    <script src="/regi/notify/check_notify.js"></script>
     <!-- 通知領域　ここまで -->
+    <script src="/regi/notify/check_notify.js"></script>
+    <script src="/common/set_tap_detail.js"></script>
 </body>
 <script src="/common/check_update_common.js"></script>
 
--- html/regi/order/list/updated_data.php
+++ html/regi/order/list/updated_data.php
@@ -7,11 +7,6 @@ if (!login_check()) {
 
 session_start();
 
-if (!isset($_POST["page_offset"]) || !isset($_POST["page_limit"])) {
-    http_response_code(400);
-    exit();
-}
-
 $last_update = date("Y/m/d H:i:s");
 $_SESSION["regi"]["order"]["list"]["last_update"] = $last_update;
 
@@ -20,54 +15,17 @@ try {
     require_once $_SERVER['DOCUMENT_ROOT'] . "/../classes/orders/order.php";
     $order_obj = new Order();
     $orders = $order_obj->get_all();
+    $page = "./no_list.php";
     if (!is_null($orders)) {
-
-        foreach ($orders as $order) {
-            $details = "";
-            foreach ($order->get_order_details() as $detail) {
-                $details .= "
-        <tr>
-            <td>{$detail->get_item_name()}</td>
-            <td>{$detail->get_quantity()}</td>
-        </tr>";
-            }
-            $data .= "
-<tr>
-    <td>{$order->get_order_order()->get_id()}</td>
-    <td>{$order->get_order_order()->get_date()}</td>
-    <td>
-        <table class='table table-bordered table-hover'>
-            <thead>
-                <tr>
-                    <th>品名</th>
-                    <th>数量</th>
-                </tr>
-            </thead>
-            <tbody>
-                {$details}
-            <tbody>
-        </table>
-    </td>
-    <td>
-        <form action='../receive/' method='post'>
-            <input type='hidden' name='order_id' id='order_id'
-                value='{$order->get_order_order()->get_id()}'>
-            <button type='submit'>受け取り</button>
-        </form>
-    </td>
-</tr>";
-        }
-    } else {
-        $data = "
-        <tr>
-            <td colspan='4'>
-                <h3>受け取り待ちの注文はありません。</h3>
-            </td>
-        </tr>
-        ";
+        $page = "./list.php";
     }
+    ob_start();
+    require $page;
+    $data = ob_get_contents();
+    ob_end_clean();
+
     echo json_encode([
-        "time" => $last_update,
+        "last-update" => $last_update,
         "table" => $data,
     ]);
     exit();
