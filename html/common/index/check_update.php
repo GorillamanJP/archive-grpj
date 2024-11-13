8ad565fc96cfb549ab577f1d2129d826e0ba0c33
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-11-13 01:47:48 +0000

    更新の有無を通知で判断するように最適化しました

--- html/common/index/check_update.php
+++ html/common/index/check_update.php
@@ -8,51 +8,16 @@ if (!isset($_SESSION["list"]["last_update"]) || $_SESSION["list"]["last_update"]
 $last_update = $_SESSION["list"]["last_update"];
 
 try {
-    $password = getenv("DB_PASSWORD");
-    $db_name = getenv("DB_DATABASE");
-    $dsn = "mysql:host=mariadb;dbname={$db_name}";
-    $pdo = new PDO($dsn, "root", $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/../classes/notifications/notification.php";
+    $notification = new Notification();
+    $notification_count = $notification->gets_notifications_after($last_update);
 
-    $sql_items = "SELECT COUNT(*) FROM items WHERE last_update >= :last_update";
+    $is_update = !is_null($notification_count);
 
-    $stmt_items = $pdo->prepare($sql_items);
-
-    $stmt_items->bindValue(":last_update", $last_update, PDO::PARAM_STR);
-
-    $stmt_items->execute();
-
-    $update_count = intval($stmt_items->fetchColumn());
-
-
-    $sql_stocks = "SELECT COUNT(*) FROM stocks WHERE last_update >= :last_update";
-
-    $stmt_stocks = $pdo->prepare($sql_stocks);
-
-    $stmt_stocks->bindValue(":last_update", $last_update, PDO::PARAM_STR);
-
-    $stmt_stocks->execute();
-
-    $update_count += intval($stmt_stocks->fetchColumn());
-
-
-    $sql_accountants = "SELECT COUNT(*) FROM accountants WHERE date >= :last_update";
-
-    $stmt_accountants = $pdo->prepare($sql_accountants);
-
-    $stmt_accountants->bindValue(":last_update", $last_update, PDO::PARAM_STR);
-
-    $stmt_accountants->execute();
-
-    $update_count += intval($stmt_accountants->fetchColumn());
-
-    $is_update = false;
-    if ($update_count > 0) {
-        $is_update = true;
-    }
     echo json_encode(["is_update" => $is_update]);
     exit();
 } catch (\Throwable $th) {
     http_response_code(500);
+    // echo json_encode(["Error" => $th->getMessage()]);
     exit();
 }
