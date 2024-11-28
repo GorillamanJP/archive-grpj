8c24e0f74e4ecf284ca3f1b6cd5fa28ffe582eca
kobayashi10101 <js22030@anabuki-net.ne.jp>
2024-11-28 06:14:39 +0000

    スマホでモバイルオーダーする際に、ボタンを連打しても画面が拡大されないように変更

--- html/order/index.php
+++ html/order/index.php
@@ -10,7 +10,7 @@ unset($_SESSION["order"]["order_items"]);
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>モバイルオーダー</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -19,7 +19,12 @@ unset($_SESSION["order"]["order_items"]);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/common/regi.css">
 </head>
-
+<style>
+html, body {
+    touch-action: pan-x pan-y; /* スクロールやスライドは有効にし、ズームは無効にする */
+    -webkit-user-select: none; /* テキスト選択を無効にする */
+}
+</style>
 <body>
     <h1 class="text-center my-3">モバイルオーダー</h1>
     <div class="container regia">
