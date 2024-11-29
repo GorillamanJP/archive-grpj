97fa5d49904bd1877adc4fe378d3f3858138e2fd
T-Gorilla(Desktop) <js22007@anabuki-net.ne.jp>
2024-11-29 21:33:02 +0900

    CAPTCHAを操作できるフラグを追加しました

--- html/order/captcha/require_captcha.php
+++ html/order/captcha/require_captcha.php
@@ -1,13 +1,15 @@
 <?php
-session_start();
-if (!isset($_SESSION["order"]["captcha"]["success"]) || $_SESSION["order"]["captcha"]["success"] === false) {
-    if (!empty($_POST)) {
-        $_SESSION["order"]["captcha"]["post_data"] = $_POST;
+if (getenv("CAPTCHA_DISABLE") != true) {
+    session_start();
+    if (!isset($_SESSION["order"]["captcha"]["success"]) || $_SESSION["order"]["captcha"]["success"] === false) {
+        if (!empty($_POST)) {
+            $_SESSION["order"]["captcha"]["post_data"] = $_POST;
+        }
+        $_SESSION["order"]["captcha"]["before"]["url"] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "/order/";
+        $_SESSION["order"]["captcha"]["after"]["url"] = $_SERVER['REQUEST_URI'];
+        session_write_close();
+        header("Location: /order/captcha/");
+        exit();
     }
-    $_SESSION["order"]["captcha"]["before"]["url"] = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "/order/";
-    $_SESSION["order"]["captcha"]["after"]["url"] = $_SERVER['REQUEST_URI'];
     session_write_close();
-    header("Location: /order/captcha/");
-    exit();
 }
-session_write_close();
