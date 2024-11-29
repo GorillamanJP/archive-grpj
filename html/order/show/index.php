0bd5df6b50085e0f58958a37cbd656473717aa0a
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-11-29 02:11:22 +0000

    さっきのコミットの加え忘れ

--- html/order/show/index.php
+++ html/order/show/index.php
@@ -34,11 +34,7 @@ if ($order->get_order_order()->get_is_received()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>注文表示</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-        crossorigin="anonymous"></script>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/common/header.php"; ?>
     <link rel="stylesheet" href="/common/list.css">
     <link rel="stylesheet" href="/common/create.css">
     <style>
