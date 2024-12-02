806b30c30b1a2b9c6d95276267ed6b377f19a402
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2024-12-02 05:16:06 +0000

    総売上一覧の商品が削除済みか分かるようになりました

--- html/regi/sales/total/sales_list.php
+++ html/regi/sales/total/sales_list.php
@@ -1,6 +1,6 @@
 <?php foreach($products as $product): ?>
     <tr>
-        <th><?= $product->get_item_name() ?></th>
+        <th><?= $product->get_item_name() ?><?= $product->get_delete_flag() ? "（削除済み）" : ""?></th>
         <td><?= $product->get_total_sold() ?></td>
         <td><?= $product->get_total_revenue() ?></td>
     </tr>
