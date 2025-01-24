25aca761eb7b9fb94510aac4fba4be8f5c649a66
T-Gorilla(Laptop-WSL) <js22007@anabuki-net.ne.jp>
2025-01-24 05:10:15 +0000

    タイトルの書式をほかと揃えました

--- html/regi/manual/index.php
+++ html/regi/manual/index.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>レジ機能ユーザーマニュアル</title>
+    <title>レジ/マニュアル</title>
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/common/header.php"; ?>
     <link rel="stylesheet" href="/common/manual.css">
 </head>
